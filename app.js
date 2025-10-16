4ff00cc8b702c957bd439ea76f7d80e4c726ab66
Ariel <xuliy100@gmail.com>
2025-10-16 14:29:55 +0000

    smt

--- app.js
+++ app.js
@@ -8,5 +8,7 @@ const server = http.createServer(function(req, res){
 server.listen(port, function(error) {
     if(error){
         console.log('Something went wrong')
+    }else{
+        console.log('Server is listening on port' + port)
     }
 })
